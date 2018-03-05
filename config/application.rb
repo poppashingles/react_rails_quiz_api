1224c3f2d5e86eaed79c9513a1335e4bc534d9ec
Rob <robdwilliams85@gmail.com>
2018-03-05 05:55:32 +0000

    Amended application configuration to allow cross-browser access

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module QuizApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'https://secure-cove-56914.herokuapp.com'
         resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
