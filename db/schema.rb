5d1e3add18c2cf852c12e37437503e0737701367
Rob <robdwilliams85@gmail.com>
2018-03-05 05:20:00 +0000

    Updated to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180221120154) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "answer"
     t.integer "points"
